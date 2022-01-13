ac224dbce0b98d99268c50892611a138f0fe0869
Rick Robertson <rickrobs7@gmail.com>
2022-01-13 15:07:45 -0700

    Renamed classes

--- js/script.js
+++ js/script.js
@@ -1,37 +1,37 @@
 $("document").ready(function () {
-  $(".product-name[data-type='mineralwater']").css(
+  $(".project-name[data-type='mineralwater']").css(
     "background-color",
     "#5b6ccf"
   );
-  $(".product-name[data-type='vitamin']").css("background-color", "#2f6e3f");
-  $(".product-name[data-type='proteinbar']").css("background-color", "#602c70");
+  $(".project-name[data-type='vitamin']").css("background-color", "#2f6e3f");
+  $(".project-name[data-type='proteinbar']").css("background-color", "#602c70");
 
   document
     .querySelector("#html-css")
     .addEventListener("change", function (evt) {
-      updateProductView("vitamins", evt.target.checked);
+      updateProjectView("vitamins", evt.target.checked);
     });
 
   document.querySelector("#reactjs").addEventListener("change", function (evt) {
-    updateProductView("mineralwater", evt.target.checked);
+    updateProjectView("mineralwater", evt.target.checked);
   });
 
   document.querySelector("#games").addEventListener("change", function (evt) {
-    updateProductView("proteinbar", evt.target.checked);
+    updateProjectView("proteinbar", evt.target.checked);
   });
 
-  $(".product-item").each(function () {
+  $(".project-item").each(function () {
     var prodName = encodeURIComponent($(this).children("h2").text());
     var prodID = encodeURIComponent($(this).data("prod_id"));
 
     var link = $(
-      "<a href='product.html?prodName=" + prodName + "&prodID=" + prodID + "'/>"
+      "<a href='project.html?prodName=" + prodName + "&prodID=" + prodID + "'/>"
     );
     $(this).children("img").wrap(link);
   });
 });
 
-function updateProductView(categoryName, bVisible) {
+function updateProjectView(categoryName, bVisible) {
   var dataSelectorVal = "";
   switch (categoryName) {
     case "vitamins":
@@ -44,7 +44,7 @@ function updateProductView(categoryName, bVisible) {
       dataSelectorVal = "h2[data-type='proteinbar']";
       break;
   }
-  $(".product-item")
+  $(".project-item")
     .has(dataSelectorVal)
     .css("display", bVisible ? "" : "none");
 }
